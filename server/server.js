3e21913bd6b0f3db865a54712d2ff1fd3707e176
Adam Wells <awells@wrayward.com>
2018-11-11 13:36:06 -0500

    had to update reference to static client files since im in new subdirectory in server.js

--- server/server.js
+++ server/server.js
@@ -35,10 +35,10 @@ require('./config/passport')(passport);
 // If we are in production serve static build and assets
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.resolve(__dirname, 'client', 'public')));
+  app.use(express.static('../client/public'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+    res.sendFile('../client/public/index.html');
   });
 }
 
